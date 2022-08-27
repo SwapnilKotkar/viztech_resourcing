49907f936ca6f022bdba6cfc03828b85728c5449
swapnil <swapnilkotkar793@gmail.com>
2022-08-27 22:25:07 +0530

    created admin and resume models

---
+++ models/resumeSchema.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+const resumeSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required:true
+    },
+    email: {
+        type: String,
+        required:true
+    },
+    mobile: {
+        type: Number,
+        required:true
+    },
+    resumeTitle: {
+        type: String,
+        required:true
+    },
+    resumeURL: {
+        type: String,
+        required:true
+    },
+    currJobTitle: {
+        type: String,
+        required:true
+    },
+    qual: {
+        type: String,
+        required:true
+    },
+    country: {
+        type: String,
+        required:true
+    }
+})
+
+
+module.exports = mongoose.models.RESUMES || mongoose.model('RESUMES', resumeSchema);
