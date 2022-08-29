1bea7d5aca95ace63374b08387020157ad67e48c
swapnil <swapnilkotkar793@gmail.com>
2022-08-29 18:33:06 +0530

    updated

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const connectDB = mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
     }).then(() => {
-    console.log(`connnection successful`);
+    console.log(`mongodb connnected successfully`);
 }).catch((err) => console.log(err));
 
 export default connectDB;
