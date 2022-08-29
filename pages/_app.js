a3f72a8da98c0960ea31b2decdc9f4fdad797746
swapnil <swapnilkotkar793@gmail.com>
2022-08-29 15:09:03 +0530

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import {LoginContext} from '../context/LoginContext'
+import Layout from '../components/Layout'
+import {Toaster} from 'react-hot-toast'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return (
+    <LoginContext>
+      <Layout>
+        <Toaster/>
+        <Component {...pageProps} />
+      </Layout>
+    </LoginContext>
+    )
 }
 
 export default MyApp
