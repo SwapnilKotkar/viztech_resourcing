f43ea8eb875a20eb9e0581479ae0cbef43798721
swapnil <swapnilkotkar793@gmail.com>
2022-08-29 23:51:54 +0530

    components updated

--- pages/api/admin.js
+++ pages/api/admin.js
@@ -8,10 +8,15 @@ const Admin = require("../../models/adminSchema");
 const handler = nc(config);
 
 
-handler.get(async(req, res) => {
+handler.post(async(req, res) => {
+    const {token} = req.body;
+    console.log(token)
     try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        console.log("token is",decoded)
+        res.status(200).json({'decoded' : decoded})
     }catch(err){
-        res.status(401).send('Unauthorized: No token provided');
+        res.status(401).json({'Unauthorized': 'No token provided'});
         console.log(err);
     }
 })
