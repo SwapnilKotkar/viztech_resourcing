4302bd9147fea3efce7454146e3ffaa2374e8c59
swapnil <swapnilkotkar793@gmail.com>
2022-08-31 21:39:56 +0530

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,16 @@ import { LoginContext } from "../context/LoginContext";
 import { ViewContext } from "../context/viewContext";
 import Layout from "../components/Layout";
 import { Toaster } from "react-hot-toast";
-import { AdminDataContext } from "../context/AdminDataContext";
 
 function MyApp({ Component, pageProps }) {
   return (
     <LoginContext>
-      <AdminDataContext>
         <ViewContext>
           <Layout>
             <Toaster />
             <Component {...pageProps} />
           </Layout>
         </ViewContext>
-      </AdminDataContext>
     </LoginContext>
   );
 }
