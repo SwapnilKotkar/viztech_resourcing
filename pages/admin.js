778c95aacde5954bb5bad54903b4fef5574ead42
swapnil <swapnilkotkar793@gmail.com>
2022-09-01 11:33:06 +0530

    fetched resumes using useEffect

--- pages/admin.js
+++ pages/admin.js
@@ -5,33 +5,38 @@ import toast from "react-hot-toast";
 import SubMenu from "../components/SubMenu";
 import { View } from "../components/View";
 
-
-const Admin = ({ resumeData }) => {
-
+const Admin = () => { //{ resumeData }
   const router = useRouter();
+  const [resumes, setResumes] = useState()
 
   useEffect(() => {
     const tokenData = localStorage.getItem("signin_token");
     if (!tokenData) {
-      // router.push('/') 
-      window.location.replace('/')
-    }  
-  }, [])
+      // router.push('/')
+      window.location.replace("/");
+    }
+
+    async function fetchResume() {
+      const resuemResponse = await fetch("http://localhost:3000/api/resumes");
+      const resumeData = await resuemResponse.json();
+      setResumes(resumeData)
+    }
+    fetchResume();
+  }, []);
 
   return (
     <>
-      <SubMenu/>
-      <View resumeData={resumeData} />
+      <SubMenu />
+      <View resumeData={resumes} />
     </>
-  )
+  );
 };
 
+// export async function getServerSideProps() {
+//   const resuemResponse = await fetch("http://localhost:3000/api/resumes");
+//   const resumeData = await resuemResponse.json();
 
-export async function getServerSideProps() {
-  const resuemResponse = await fetch("http://localhost:3000/api/resumes");
-  const resumeData = await resuemResponse.json();
-
-  return { props: { resumeData } };
-}
+//   return { props: { resumeData } };
+// }
 
 export default Admin;
