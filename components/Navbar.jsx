a0f3af02cf340884ab6b73a1b9d670efd1403c2f
swapnil <swapnilkotkar793@gmail.com>
2022-09-02 19:10:42 +0530

    navbar fixed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,142 +1,150 @@
-import { Fragment } from "react";
+import React, { useRef, useState } from "react";
 import Link from "next/link";
-import { Popover, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useLoginContext } from "../context/LoginContext";
 
 const Navbar = () => {
   const { loginStatus, handleLogout } = useLoginContext();
+  const ref = useRef();
+  const [menu, setMenu] = useState("hidden")
+
+  const menuList = [
+    { name: "Home", href: "/" },
+    { name: "About Us", href: "/#about" },
+    { name: "Apply now", href: "/apply" },
+  ];
+
+  const menuOpen = () => {
+    if (menu == "hidden") {
+      setMenu("block")
+    }
+  };
+
+  const menuClose = () => {
+    if (menu == "block") {
+      setMenu("hidden")
+    }
+  };
 
   return (
-    <>
-      <Popover className="relative bg-white">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6">
-          <div className="flex justify-between items-center border-gray-100 py-6 md:justify-start md:space-x-10">
-            <div className="flex justify-start lg:w-0 lg:flex-1">
-              <a href="#">
-                <span className="sr-only">Workflow</span>
-                <img
-                  className="h-8 w-auto sm:h-10"
-                  src="https://tailwindui.com/img/logos/workflow-mark.svg?color=gray&shade=900"
-                  alt=""
-                />
-              </a>
+    <div className=" relative bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-between items-center border-gray-100 py-6 md:justify-start md:space-x-10">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <a href="#">
+              <span className="sr-only">Viztech Logo</span>
+              <img
+                className="h-8 w-auto sm:h-10"
+                src="https://tailwindui.com/img/logos/workflow-mark.svg?color=gray&shade=900"
+                alt=""
+              />
+            </a>
+          </div>
+          <div className="-mr-2 -my-2 md:hidden">
+            <div
+              onClick={menuOpen}
+              className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-900"
+            >
+              <span className="sr-only">Open menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" color="black" />
             </div>
-            <div className="-mr-2 -my-2 md:hidden">
-              <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-900">
-                <span className="sr-only">Open menu</span>
-                <Bars3Icon className="h-6 w-6" aria-hidden="true" color="black" />
-              </Popover.Button>
-            </div>
-            <Popover.Group as="nav" className="hidden md:flex space-x-10">
-              <Link href="/">
+          </div>
+          <div as="nav" className="hidden md:flex space-x-10">
+            {menuList.map((item) => (
+              <Link key={item.name} href={item.href}>
                 <a className="text-base font-medium text-gray-500 hover:text-gray-900">
-                  Home
+                  {item.name}
                 </a>
               </Link>
-              <Link href="/#about">
-                <a className="text-base font-medium text-gray-500 hover:text-gray-900">
-                  About Us
-                </a>
-              </Link>
-              <Link href="/apply">
-                <a className="text-base font-medium text-gray-500 hover:text-gray-900">
-                  Apply Now
-                </a>
-              </Link>
-            </Popover.Group>
-            <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-              {loginStatus && (
-                <>
-                  <Link href="/admin">
-                    <a className="text-base font-medium text-gray-500 hover:text-gray-900">
-                      Dashboard
-                    </a>
-                  </Link>
-                  <Link href="/">
-                    <a
-                      href="#"
-                      className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded shadow-sm text-base font-medium text-white bg-gray-900 hover:bg-gray-900"
-                      onClick={handleLogout}
-                    >
-                      Log out
-                    </a>
-                  </Link>
-                </>
-              )}
-              {!loginStatus && (
-                <Link href="/signin">
+            ))}
+          </div>
+          <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+            {loginStatus && (
+              <>
+                <Link href="/admin">
+                  <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+                    Dashboard
+                  </a>
+                </Link>
+                <Link href="/">
                   <a
                     href="#"
                     className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded shadow-sm text-base font-medium text-white bg-gray-900 hover:bg-gray-900"
+                    onClick={handleLogout}
                   >
-                    Admin Sign in
+                    Log out
                   </a>
                 </Link>
-              )}
-            </div>
+              </>
+            )}
+            {!loginStatus && (
+              <Link href="/signin">
+                <a
+                  href="#"
+                  className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded shadow-sm text-base font-medium text-white bg-gray-900 hover:bg-gray-900"
+                >
+                  Admin Sign in
+                </a>
+              </Link>
+            )}
           </div>
         </div>
-
-        <Transition
-          as={Fragment}
-          enter="duration-200 ease-out"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="duration-100 ease-in"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <Popover.Panel
-            focus
-            className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-          >
-            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-              <div className="pt-5 pb-6 px-5">
-                <div className="flex items-center justify-between">
-                  <div>
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-mark.svg?color=gray&shade=900"
-                      alt="Workflow"
-                    />
-                  </div>
-                  <div className="-mr-2">
-                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-900">
-                      <span className="sr-only">Close menu</span>
-                      <XMarkIcon className="h-6 w-6" aria-hidden="true" color="black" />
-                    </Popover.Button>
-                  </div>
-                </div>
-                <div className="mt-6">
-                  <nav className="grid gap-y-8">
-                    <Link href="/">
-                      <a className="text-base font-medium text-gray-700 hover:text-gray-900">
-                        Home
-                      </a>
-                    </Link>
-                    <Link href="/#about">
-                      <a className="text-base font-medium text-gray-700 hover:text-gray-900">
-                        About Us
-                      </a>
-                    </Link>
-                    <Link href="/apply">
-                      <a className="text-base font-medium text-gray-700 hover:text-gray-900">
-                        Apply Now
-                      </a>
-                    </Link>
-                  </nav>
+      </div>
+      <div
+        ref={ref}
+        focus
+        className={`absolute top-0 inset-x-0 p-2 transition transform origin-top-right ${menu} md:hidden z-10`}
+      >
+        <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+          <div className="pt-5 pb-6 px-5">
+            <div className="flex items-center justify-between">
+              <div>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/workflow-mark.svg?color=gray&shade=900"
+                  alt="Workflow"
+                />
+              </div>
+              <div className="-mr-2">
+                <div
+                  onClick={menuClose}
+                  className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-900"
+                >
+                  <span className="sr-only">Close menu</span>
+                  <XMarkIcon
+                    className="h-6 w-6"
+                    aria-hidden="true"
+                    color="black"
+                  />
                 </div>
               </div>
-              <div className="py-6 px-5 space-y-6">
-              {loginStatus && (
-                <>
-                  <Link href="/admin">
+            </div>
+            <div className="mt-6">
+              <nav className="grid gap-y-8">
+                {menuList.map((item) => (
+                  <Link key={item.name} href={item.href}>
+                    <div onClick={menuClose}>
+                      <a className="text-base font-medium text-gray-700 hover:text-gray-900">
+                        {item.name}
+                      </a>
+                    </div>
+                  </Link>
+                ))}
+              </nav>
+            </div>
+          </div>
+          <div className="py-6 px-5 space-y-6">
+            {loginStatus && (
+              <>
+                <Link href="/admin">
+                  <div onClick={menuClose}>
                     <a className="text-base font-medium text-gray-700 hover:text-gray-900">
                       Dashboard
                     </a>
-                  </Link>
-                  <Link href="/">
+                  </div>
+                </Link>
+                <Link href="/">
+                  <div onClick={menuClose}>
                     <a
                       href="#"
                       className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded shadow-sm text-base font-medium text-white bg-gray-900 hover:bg-gray-900"
@@ -144,25 +152,24 @@ const Navbar = () => {
                     >
                       Log out
                     </a>
-                  </Link>
-                </>
-              )}
-              {!loginStatus && (
-                <Link href="/signin">
-                    <a className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded shadow-sm text-base font-medium text-white bg-gray-900 hover:bg-gray-900">
-                      Admin Sign in
-                    </a>
-                  </Link>
-              )}
-                <div>
-                  
+                  </div>
+                </Link>
+              </>
+            )}
+            {!loginStatus && (
+              <Link href="/signin">
+                <div onClick={menuClose}>
+                  <a className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded shadow-sm text-base font-medium text-white bg-gray-900 hover:bg-gray-900">
+                    Admin Sign in
+                  </a>
                 </div>
-              </div>
-            </div>
-          </Popover.Panel>
-        </Transition>
-      </Popover>
-    </>
+              </Link>
+            )}
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
