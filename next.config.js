e771f65c960dd93e38be030cf8739b592d9ab90c
swapnil <swapnilkotkar793@gmail.com>
2022-09-09 14:19:57 +0530

    changed env names

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   swcMinify: true,
   env:{
     CRYPTO_SECRET_KEY : process.env.CRYPTO_SECRET_KEY,
-    AWS_ACCESS_KEY_ID : process.env.AWS_ACCESS_KEY_ID,
-    AWS_SECRET_ACCESS_KEY : process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_ACCESS_KEY_NAME : process.env.AWS_ACCESS_KEY_NAME,
+    AWS_SECRET_ACCESS_KEY_NAME : process.env.AWS_SECRET_ACCESS_KEY_NAME,
     AWS_BUCKET_NAME : process.env.AWS_BUCKET_NAME,
     AWS_BUCKET_REGION : process.env.AWS_BUCKET_REGION
   }
--- pages/apply.js
+++ pages/apply.js
@@ -20,8 +20,8 @@ const Apply = () => {
   const config = {
     bucketName: process.env.AWS_BUCKET_NAME,
     region: process.env.AWS_BUCKET_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_NAME,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_NAME,
 }
 
   const handleFileChange = async (event) => {
